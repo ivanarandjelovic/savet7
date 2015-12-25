fde9fb37f5921d5cc944cec6a6401724a353622e
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2015-12-25 14:07:28 +0100

    Removed unneeded code.

--- src/main/java/org/aivan/savet7/config/SecurityConfiguration.java
+++ src/main/java/org/aivan/savet7/config/SecurityConfiguration.java
@@ -6,7 +6,6 @@ import org.aivan.savet7.security.RESTAuthenticationFailureHandler;
 import org.aivan.savet7.security.RESTAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -45,14 +44,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	 * ProviderManager( { userDetailsService}); retur pm }
 	 */
 
-	private static class NopPostProcessor implements ObjectPostProcessor {
-		@Override
-		@SuppressWarnings("unchecked")
-		public Object postProcess(Object object) {
-			return object;
-		}
-	};
-
+	/*
+	 * private static class NopPostProcessor implements ObjectPostProcessor {
+	 * 
+	 * @Override
+	 * 
+	 * @SuppressWarnings("unchecked") public Object postProcess(Object object) {
+	 * return object; } };
+	 */
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
@@ -60,14 +59,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		//web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/fonts/**", "/*.html", "/",
-		//		"/partials/**");
+		// web.ignoring().antMatchers("/css/**", "/js/**", "/img/**",
+		// "/fonts/**", "/*.html", "/",
+		// "/partials/**");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin().successHandler(authenticationSuccessHandler).failureHandler(authenticationFailureHandler).and()
-				.csrf().disable().authorizeRequests().antMatchers("/api/**","/userService/**")
+				.csrf().disable().authorizeRequests().antMatchers("/api/**", "/userService/**")
 				.access("hasRole('USER') or hasRole('ADMIN')").and().exceptionHandling()
 				.authenticationEntryPoint(authenticationEntryPoint);
 
