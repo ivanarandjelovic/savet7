e0951c3423609ff3bc395e70ebc5f99aa5c23e37
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2015-12-27 00:36:37 +0100

    login and logout work

--- src/main/resources/public/js/controllers/BuildingListCtrl.js
+++ src/main/resources/public/js/controllers/BuildingListCtrl.js
@@ -1,20 +1,29 @@
 savet7App.controller('BuildingListCtrl', function($scope, $http) {
 
-	$http.get('/api/buildings').then(function successCallback(response) {
-		$scope.buildings = response.data._embedded.buildings;
-	}, function errorCallback(response) {
-		if(response.status === 401) {
-			// we are not logged in
-			// TODO ...
+	$scope.loadBuildings = function() {
+		if ($scope.$parent && $scope.$parent.loggedIn === true) {
+			$http.get('/api/buildings').then(
+					function successCallback(response) {
+						$scope.buildings = response.data._embedded.buildings;
+					}, function errorCallback(response) {
+						if (response.status === 401) {
+							// we are not logged in
+							// TODO ...
+						}
+					});
 		}
-	});
+	};
 
+	$scope.loadBuildings();
+	$scope.$parent.$watch('loggedIn', $scope.loadBuildings);
 });
 
-savet7App.controller('BuildingDetailCtrl', function($scope, $http, $routeParams) {
+savet7App.controller('BuildingDetailCtrl',
+		function($scope, $http, $routeParams) {
 
-	$http.get('/api/buildings/'+$routeParams.buildingId).success(function(data) {
-		$scope.building = data;
-	});
+			$http.get('/api/buildings/' + $routeParams.buildingId).success(
+					function(data) {
+						$scope.building = data;
+					});
 
-});
+		});
--- src/main/resources/public/js/controllers/UserCtrl.js
+++ src/main/resources/public/js/controllers/UserCtrl.js
@@ -1,17 +1,23 @@
-savet7App.controller('UserCtrl', function($scope, $http, $uibModal, $log) {
+savet7App.controller('UserCtrl', function($scope, $http, $uibModal, $log, $location) {
 
-	$http.get('/userService/get').then(function successCallback(response) {
-		$scope.user = response.data;
-		$scope.loggedIn = true;
-	}, function errorCallback(response) {
-		if (response.status === 401) {
-			// we are not logged in
-			// TODO ...
-			$scope.user = null;
-			$scope.loggedIn = false;
-		}
-	});
-
+	$scope.loggedIn = false;
+	
+	$scope.getUser = function() {
+		$http.get('/userService/get').then(function successCallback(response) {
+			$scope.user = response.data;
+			$scope.loggedIn = true;
+			toastr.success('Are you logged in');
+			
+		}, function errorCallback(response) {
+			if (response.status === 401) {
+				// we are not logged in
+				// TODO ...
+				$scope.user = null;
+				$scope.loggedIn = false;
+			}
+		});
+	}
+	
 	$scope.login = function() {
 		var modalInstance = $uibModal.open({
 			animation : true,
@@ -26,19 +32,49 @@ savet7App.controller('UserCtrl', function($scope, $http, $uibModal, $log) {
 		modalInstance.result.then(function(modalResult) {
 			// Login was performed ///
 			// TODO: what now? reload user, or simply re-load page?
+			$scope.getUser();
 		}, function() {
 			$log.info('Login dismissed at: ' + new Date());
 		});
 	};
 
+	$scope.logout = function() {
+		$http.get('/logout').then(function successCallback(response) {
+			$scope.user = null;
+			$scope.loggedIn = false;
+			toastr.warning('Are you logged out');
+		});
+		$location.path( "/" );
+	}
+	
+	$scope.getUser();
+	
 });
 
 savet7App.controller('LoginInstanceCtrl', function($scope, $http,
 		$uibModalInstance) {
 	$scope.login = function() {
 //		$uibModalInstance.close($scope.selected.item);
-		$uibModalInstance.close(1);
-		// TODO: do login here
+		//alert($scope.username);
+		//alert($scope.password);
+        var config = {
+                headers : {
+                    'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            };
+        
+        var data = $.param({
+        	username: $scope.username,
+        	password: $scope.password
+        });
+        
+		$http.post('/login',data, config).then(function successCallback(response) {
+			// Yup, we are logged in
+			$uibModalInstance.close(1);
+		}, function errorCallback(response) {
+			// Login error, show message:
+			alert("Bad login!");
+		});
 	};
 
 	$scope.cancel = function() {
