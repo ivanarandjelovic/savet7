f81bfc93a3c4e30e6f5be0e056e35d44d94cb8cd
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2016-01-06 18:25:03 +0100

    Filter for slowing down api requests (for testing purposes).

--- src/main/java/org/aivan/savet7/Savet7Main.java
+++ src/main/java/org/aivan/savet7/Savet7Main.java
@@ -1,9 +1,12 @@
 package org.aivan.savet7;
 
+import org.aivan.savet7.filter.SlowFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Savet7Main extends SpringBootServletInitializer {
@@ -17,4 +20,12 @@ public class Savet7Main extends SpringBootServletInitializer {
 		SpringApplication.run(Savet7Main.class, args);
 	}
 
+/*	@Bean
+	public FilterRegistrationBean filterRegistrationBean() {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		SlowFilter slowFilter = new SlowFilter();
+		registrationBean.setFilter(slowFilter);
+		registrationBean.setOrder(1);
+		return registrationBean;
+	}*/
 }
---
+++ src/main/java/org/aivan/savet7/filter/SlowFilter.java
@@ -0,0 +1,56 @@
+package org.aivan.savet7.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+public class SlowFilter implements javax.servlet.Filter {
+
+	private static final long SLOW_DOWN_MS = 1000;
+
+	public FilterConfig filterConfig;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.filterConfig = filterConfig;
+	}
+
+	/**
+	 * Development filter to slow down each request, for testing UI in such
+	 * conditions
+	 * 
+	 * @param request
+	 * @param response
+	 * @param chain
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+
+		String url = ((HttpServletRequest) request).getRequestURL().toString();
+
+		if (url.contains("/api")) {
+			try {
+				System.out.println("Slowing down ... for " + SLOW_DOWN_MS + " milliseconds.");
+				Thread.sleep(SLOW_DOWN_MS);
+			} catch (InterruptedException ie) {
+				System.out.println("Interrupted!");
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
