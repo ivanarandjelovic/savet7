ac942314b708e1153f557dfc6248fe2386cc8ced
Ivan Arandjelovic <ivan.arandjelovic@crealogix.com>
2016-01-08 12:26:15 +0100

    Better test data, list changed to table, pager translated, etc.

--- src/main/resources/public/js/directives/s7Building.js
+++ src/main/resources/public/js/directives/s7Building.js
@@ -4,6 +4,7 @@ savet7App.directive('s7Building', function() {
 		templateUrl: '../partials/s7Building.html',
 		scope: {
 			building: '=building'
-		}
+		},
+		replace: true
 	}
 });
---
+++ src/test/java/org/aivan/savet7/TestDataGenerator.java
@@ -0,0 +1,13 @@
+package org.aivan.savet7;
+
+public class TestDataGenerator {
+	private static final int TEST_DATA_COUNT = 77;
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= TEST_DATA_COUNT; i++) {
+			System.out.println(
+					"insert into address (id,street, number,apartment, postalCode, city, state, country) values (" + i + ",'Kapetana Miloša Žunjića','35A','" + i + "','11160','Beograd','','Srbija');");
+			System.out.println("insert into building (name, address_id) values ('Test building " + i + "'," + i + ");");
+		}
+	}
+}
