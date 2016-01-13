e5a9b66070757229488564d6be741694db30ad72
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2016-01-14 00:46:26 +0100

    Fixed exception logging

--- src/main/java/org/aivan/savet7/security/RESTLogoutHandler.java
+++ src/main/java/org/aivan/savet7/security/RESTLogoutHandler.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
@@ -12,13 +13,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class RESTLogoutHandler implements LogoutHandler {
 
+	static Logger log = Logger.getLogger(RESTLogoutHandler.class);
+	
 	@Override
 	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
 		// TODO Auto-generated method stub
 		try {
 			response.sendError(HttpServletResponse.SC_OK);
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.warn("IOException when sending response code for successfull login", e);
 			throw new RuntimeException(e);
 		}
 	}
