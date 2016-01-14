cb9bad47e379e61253f3242e5e2130f15a7b29de
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2016-01-14 23:32:52 +0100

    fix

--- src/main/java/org/aivan/savet7/security/SecurityException.java
+++ src/main/java/org/aivan/savet7/security/SecurityException.java
@@ -2,23 +2,13 @@ package org.aivan.savet7.security;
 
 public class SecurityException extends RuntimeException {
 
-	public SecurityException() {
-	}
-
-	public SecurityException(String message) {
-		super(message);
-	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public SecurityException(Throwable cause) {
 		super(cause);
-	}
-
-	public SecurityException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
-	public SecurityException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
 	}
 
 }
