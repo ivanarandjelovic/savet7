b58f4ab990cdc8a86b5c91f19b15c0314bf594f0
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2016-01-22 23:35:52 +0100

    No use of trying to get e2e reports to Sonar (probably no need really).

--- protractor-heroku.conf.js
+++ protractor-heroku.conf.js
@@ -2,21 +2,21 @@ var jasmineReporters = require('jasmine-reporters');
 var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 exports.config = {
-  //seleniumAddress : 'http://localhost:4444/wd/hub',
-  specs : [ 'arc/test/e2e/util/**.js','src/test/e2e/*-spec.js' ],
+  // seleniumAddress : 'http://localhost:4444/wd/hub',
+  specs : [ 'arc/test/e2e/util/**.js', 'src/test/e2e/*-spec.js' ],
   framework : "jasmine2",
   onPrepare : function() {
     jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
-      savePath : 'build/test-results/js',
+      savePath : 'build/test-results/e2e',
       consolidateAll : true
     }));
     jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
       savePath : 'build/reports/e2e/',
-      takeScreenshots: true,
-      screenshotsFolder: 'images'
+      takeScreenshots : true,
+      screenshotsFolder : 'images'
     }));
   },
   params : {
-    baseUrl: "http://pacific-gorge-58447.herokuapp.com/"
+    baseUrl : "http://pacific-gorge-58447.herokuapp.com/"
   }
 };
