ec5161b1deba7172f68cf19110fca742edddb5f5
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2016-01-24 02:38:51 +0100

    Adding and editing building tests added, adding address as well.

--- src/test/e2e/buildings-spec.js
+++ src/test/e2e/buildings-spec.js
@@ -6,7 +6,14 @@ describe('savet7 buildings test', function() {
   var loginLink = element(by.id("loginLink"));
   var logoutLink = element(by.id("logoutLink"));
   var usernameInNav = element(by.id("usernameInNav"));
-
+  var nameField = element(by.id('name'));
+  var saveButton = element(by.css('[ng-click="submit()"]'));
+  var cancelButton = element(by.css('[ng-click="cancel()"]'));
+  var editBuildingButton = element(by.css('[ng-click="edit()"]'));
+  var editAddressButton = element(by.css('[ng-click="editAddress()"]'));
+  var streetField = element(by.model('address.street'));
+  var backButton = element(by.linkText('Back'));
+  
   var open = function() {
     // browser.get('https://pacific-gorge-58447.herokuapp.com/');
     browser.get(browser.params.baseUrl);
@@ -57,11 +64,42 @@ describe('savet7 buildings test', function() {
     });
   });
 
-  fit('should edit one building', function(done) {
+  it('should add new building with error', function(done) {
     loggedInTest(done, function() {
-      element(by.linkText('Add building')).click().then(function() {
-        expect(element(by.css('h2')).getText()).toBe('Add new building:');
-      });
+      element(by.linkText('Add building')).click();
+      expect(element(by.css('h2')).getText()).toBe('Add new building:');
+      nameField.sendKeys('shor');
+      expect(element(by.css('[ng-show="showError(\'name\')"]')).getInnerHtml()).toContain(
+          'Field is too short or missing.');
+      saveButton.click();
+      expect(element(by.css('[ng-show="showFormError"]')).element(by.css('.with-errors')).getInnerHtml()).toContain(
+          'Form submission failed, please see details below.');
+      cancelButton.click();
+      expect(element(by.tagName('h3')).getText()).toBe('Building list');
+    });
+  });
+
+  it('should add new building then edit it', function(done) {
+    loggedInTest(done, function() {
+      element(by.linkText('Add building')).click();
+      expect(element(by.css('h2')).getText()).toBe('Add new building:');
+      nameField.sendKeys('Long name');
+      expect(element(by.css('[ng-show="showError(\'name\')"]')).isDisplayed()).toBeFalsy();
+      saveButton.click();
+      expect(element(by.css('.panel-title')).getText()).toBe('Building details');
+      expect(element(by.binding('building.name')).getText()).toBe('Long name');
+      editBuildingButton.click();
+      nameField.sendKeys('-2');
+      saveButton.click();
+      expect(element(by.css('.panel-title')).getText()).toBe('Building details');
+      expect(element(by.binding('building.name')).getText()).toBe('Long name-2');
+      editAddressButton.click();
+      streetField.sendKeys('address 12345');
+      saveButton.click();
+      expect(element(by.css('.panel-title')).getText()).toBe('Building details');
+      expect(element(by.binding('address.street')).getText()).toBe('address 12345');
+      backButton.click();
+      expect(element(by.tagName('h3')).getText()).toBe('Building list');
     });
   });
 
