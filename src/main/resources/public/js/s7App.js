257b784efcad3ea77062889a28a2c533dfe8b91f
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2016-01-27 19:37:06 +0100

    Starts to look good

--- src/main/resources/public/js/s7App.js
+++ src/main/resources/public/js/s7App.js
@@ -2,44 +2,42 @@ var S7App = React.createClass({
   render: function() {
     return (
       <div>
-      <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        /* Brand and toggle get grouped for better mobile display */
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed"
+      <nav className="navbar navbar-default">
+      <div className="container-fluid">
+        {/* Brand and toggle get grouped for better mobile display */}
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed"
             data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
             aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span> <span
-              class="icon-bar"></span> <span class="icon-bar"></span> <span
-              class="icon-bar"></span>
+            <span className="sr-only">Toggle navigation</span> <span
+            className="icon-bar"></span> <span className="icon-bar"></span> <span
+            className="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Savet7</a>
+          <a className="navbar-brand" href="#">Savet7</a>
         </div>
 
-        /* Collect the nav links, forms, and other content for toggling */
-        <div class="collapse navbar-collapse"
-          id="bs-example-navbar-collapse-1" ng-controller="userCtrl">
-          <ul class="nav navbar-nav navbar-right">
+        {/* Collect the nav links, forms, and other content for toggling */}
+        <div className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right">
             <li ng-if="!loggedIn"><a href="" ng-click="login()"
               id="loginLink">Login</a></li>
             <li ng-if="loggedIn"><a href="" ng-click="logout()"
               id="logoutLink">Logout</a></li>
 
 
-          <p class="navbar-text navbar-right" ng-if="loggedIn">
+          <p className="navbar-text navbar-right" ng-if="loggedIn">
             Logged in as <b id="usernameInNav">username</b>
           </p>
-          <ul class="nav navbar-nav navbar-right" ng-controller="langCtrl">
+          <ul className="nav navbar-nav navbar-right" ng-controller="langCtrl">
             
-            <li class="dropdown"><a class="dropdown-toggle"
+            <li className="dropdown"><a className="dropdown-toggle"
               data-toggle="dropdown" role="button" aria-haspopup="true"
-              aria-expanded="false" id="langSelectLink">English<span class="caret"></span>
+              aria-expanded="false" id="langSelectLink">English<span className="caret"></span>
             </a>
-              <ul class="dropdown-menu">
-                <li ng-class="{active : langData.langCd == 'en'}"><a
-                  ng-click="useLang('en')">English</a></li>
-                <li ng-class="{active : langData.langCd == 'rs'}"><a
-                  ng-click="useLang('rs')">Serbian</a></li>
+              <ul className="dropdown-menu">
+                <li className="active"><a>English</a></li>
+                <li className=""><a>Serbian</a></li>
               </ul></li>
           </ul>
 				</ul>
@@ -48,7 +46,7 @@ var S7App = React.createClass({
     </nav>
 
 
-    <div class="container" ng-view></div>
+    <div className="container" ng-view></div>
     </div>
     );
   }
