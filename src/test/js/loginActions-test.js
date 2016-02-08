1388fe43ac2486db4e25c8900ac7f19d5566cb49
Ivan Arandjelovic <ivan.arandjelovic@gmail.com>
2016-02-08 23:39:13 +0100

    More tests

--- src/test/js/loginActions-test.js
+++ src/test/js/loginActions-test.js
@@ -1,16 +1,38 @@
 describe('loginActions', function() {
 
   var loginActions;
+  var userObj = {username: "test"};
+  var loginAction = {
+    type: 'LOGIN_ACTION',
+    username: 'test'
+  };
 
   beforeEach(function() {
-    this.loginActions = require.requireActual('../../main/resources/dev_public/js/actions/loginActions');
+    loginActions = require.requireActual('../../main/resources/dev_public/js/actions/loginActions');
   });
 
   it('should return action when setting username', function() {
-    expect(this.loginActions.setUsername({username: 'test'})).toEqual({
-      type: 'LOGIN_ACTION',
-      username: 'test'
-    });
+    expect(loginActions.setUsername(userObj)).toEqual(loginAction);
   });
+
+  it('should return function when asking to fetch the user', function() {
+    var action = loginActions.fetchUser();
+    expect(typeof action).toBe('function');
+    var dispatch = jest.genMockFunction();
+    action(dispatch);
+
+    var $ = require('jquery');
+    expect($.get.mock.calls.length).toEqual(1);
+    expect($.get).toBeCalledWith({
+        url: 'http://localhost:8080/userService/get',
+        xhrFields: {
+          withCredentials: true
+        }
+      }, jasmine.any(Function));
+     // Simulate return data from api call:
+     $.get.mock.calls[0 /*first call*/][1 /*first argument*/](userObj);
+     expect(dispatch).toBeCalledWith(loginAction);
+  });
+
 
 });
